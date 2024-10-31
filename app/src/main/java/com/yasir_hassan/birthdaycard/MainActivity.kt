ff9ed6174d64cd939e78a35cb1d4de4de45b7194
Yasir_Hassan <yasir.hassan7722@gmail.com>
2024-10-30 23:51:18 -0500

    add common strings to String Resource

--- app/src/main/java/com/yasir_hassan/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/yasir_hassan/birthdaycard/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,8 +33,8 @@ class MainActivity : ComponentActivity() {
             BirthDayCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     GreetingImage(
-                        message = "Happy Birthday My Friend!",
-                        from = "From Yasir",
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.from_yasir_text),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -101,7 +102,10 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
 //@Composable
 //fun BirthdayCardPreview() {
 //    BirthDayCardTheme {
-//        GreetingText("Happy B D, Friend!", from = "From Yasir")
+//        GreetingImage(
+//            message = stringResource(R.string.happy_birthday_text),
+//            from = stringResource(R.string.from_yasir_text)
+//        )
 //    }
 //}
 
